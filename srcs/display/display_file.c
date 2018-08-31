e2fe21ba048799b9c1e951f8039dc6ff746e00e8
Remsko <pinoit.remi@gmail.com>
2018-08-31 22:32:30 +0200

    clear hardcore if condit

--- srcs/display/display_file.c
+++ srcs/display/display_file.c
@@ -6,7 +6,7 @@
 /*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/28 12:59:59 by rpinoit           #+#    #+#             */
-/*   Updated: 2018/08/31 19:18:12 by rpinoit          ###   ########.fr       */
+/*   Updated: 2018/08/31 21:29:39 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void    display_file(t_target *target)
 {
+    ft_putstr(target->path);
+    ft_putstr(" -> ");
     ft_putstr(target->name);
     ft_putchar('\n');
 }
--- srcs/display/display_list.c
+++ srcs/display/display_list.c
@@ -6,11 +6,26 @@
 /*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/25 20:58:58 by rpinoit           #+#    #+#             */
-/*   Updated: 2018/08/31 19:09:10 by rpinoit          ###   ########.fr       */
+/*   Updated: 2018/08/31 21:29:21 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
+
+static t_bool   check_point(char *name)
+{
+    if (name[0] != '.')
+        return (FALSE);
+    else
+    {
+        if (name[1] == '\0')
+            return (TRUE);
+        else if (name[1] == '.' && name[2] == '\0')
+            return (TRUE);
+        else
+            return (FALSE);
+    }
+}
 
 void display_list(t_slist *list, int recu)
 {
@@ -26,9 +41,8 @@ void display_list(t_slist *list, int recu)
         target = (t_target *)list->content;
         if (target->stat.st_mode & S_IFDIR)
         {
-            if (target->name[0] != '.'
-            && (target->name[1] != '\0'
-            || (target->name[1] != '.' && target->name[2] != '\0')))
+            if (check_point(target->name) == FALSE)
+            //if (ft_strcmp(target->name, "..") != 0)
             {
                 process_dir(&directory, target->path);
                 if ((new = slist_new((void *)directory)) == NULL)
@@ -36,9 +50,10 @@ void display_list(t_slist *list, int recu)
                 slist_add_start(&directories, new);
             }
         }
-        else
-            display_file(target);
+        display_file(target);
         list = list->next;
     }
+    //if (directories != NULL)
+    //    ft_putchar('\n');
     display_directory(directories, recu + 1);
 }
